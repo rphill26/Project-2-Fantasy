fff34f16d8ca3134690e8ca4e25a0f4871184b46
Randall Phillips <rphill45@uncc.edu>
2019-12-04 11:44:20 -0500

    added login, user tables, migrations, and a public file

---
+++ controllers/authController.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = require("express").Router();
+
+const db = require("../models/index")
+
+router.get("/login", (req, res) => {
+    res.render("auth/login");
+});
+
+router.post("/login", (req, res) => {
+    db.User.findOne({ where: { email: req.body.email }})
+        .then(user => {
+            if(!user){
+                return res.render("auth/login", { error: "Invalid email or password, please try again" });
+            }
+
+            if (user.password !== req.body.password){
+                return res.render("auth/login", { error: "Invalid email or password, please try again" });
+            }
+
+            req.session.userId = user.id;
+            res.redirect("/login");
+        })
+        .catch(err => {
+            console.log(err);
+        })
+});
+
+router.get("/logout", (req, res) => {
+    req.session.destroy(err => {
+      if (err) throw err;
+      res.redirect("/");
+    });
+  });
+
+module.exports = router;
